b871d9ab25a01765b9a0631993c6b162dc9d5f06
Vithal Reddy <vmreddyvmb@gmail.com>
2017-04-11 10:56:13 +0530

    first commit

---
+++ myApp.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+  $('#mainbox').hide();
+  $('.card').hide();
+
+
+    var userFacebookToken = 'EAACEdEose0cBAKU0WTFZA3XX4Ok6ZBHCMKFSwRzQo9JEFnr669VwQRS8NQd5FXvNpIrgu0In0HgzjZBqfNKRNIBWbIZCr2mkPBiwyDgxul8VQsQrMtlXbaD4HqdsfZAzvtTZBkVqiGnmWxZA94xsyjNGq1CCeWc0KU1KrPEofAapMdIenR4SZCvTvCu8ZCgzpQFgZD';
+
+    function getFacebookInfo() {
+
+        $.ajax('https://graph.facebook.com/me?fields=id,name,picture,hometown,gender,email,feed.include_hidden(true).limit(10),location&access_token='+ userFacebookToken, {
+                success: function(response) {
+                  console.log(response);
+                 $("#myFirstName").text(response.first_name);
+                    $("#myLastName").text(response.last_name);
+                    $("#myFbName").text(response.name);
+                    $("#myEmail").text(response.email);
+                    $("#myHomeTown").text(response.hometown.name);
+                    $("#myLocation").text(response.location.name);
+                    $("#myGender").text(response.gender);
+                    $("#myProfilePic").html('<img src="https://graph.facebook.com/' + response.id + '/picture"  />');
+                    $("#myProfilePicLarge").html('<img src="https://graph.facebook.com/' + response.id + '/picture?type=large"  id="myDpx"/>');
+                    $("#myProfileId").html('<a target="blank" href="https://facebook.com/' + response.id + '">https://facebook.com/' + response.id + '</a>');
+
+                    var jData = response.feed.data;
+                    var html = "";
+                    html += "<div class='fbFeed'>"+"<ul>";
+                    $.each(jData, function(index, value) {
+                      if (value.id != undefined){
+                        html+="<li>"+"<div class='row'>";
+                        html+=  '<div class="col-md-4">'+'<img src="https://graph.facebook.com/' + response.id + '/picture" class="avatar" />';
+                        html += "<h3>" +'<a target="blank" href="https://facebook.com/' + response.id + '">' + response.name + '</a>' +"</h3>"+'</div>';
+                        html += '<div class="col-md-8">';
+                        if (response.feed.data[index].picture != undefined && response.feed.data[index].type != "link" ) {
+                        html+=  '<img src="' + response.feed.data[index].picture + '" class="postPic" />';
+                        }
+                        if (response.feed.data[index].message != undefined ) {
+                        html += '<p class="message">'  + value.message +  '</p>';
+                      }
+                        if (response.feed.data[index].link != undefined && response.feed.data[index].name != undefined ) {
+                        html += '<a target="blank" href="' + response.feed.data[index].link + '">' + response.feed.data[index].name + '</a>';
+                        }
+                        html+=' </div>'+'</div>'+"</li><hr>";
+                        }
+                    });
+                    html += "</ul>"+"</div>";
+                    $(".fbFeed").append(html);
+
+                },
+                error : function(request,errorType,errorMessage){
+                    console.log(request);
+                    console.log(errorType);
+                    alert(statusText);
+                },
+                timeout:3000,
+                beforeSend : function(){
+                    $('#mainbox').hide();
+                    $('.card').hide();
+
+
+                },
+                complete : function(){
+                   $('#mainbox').show();
+                   $('.card').show();
+
+
+
+
+
+                }
+            } //end argument list
+
+
+
+        ); // end ajax call
+
+
+    } // end get facebook info
+
+    $("#facebookBtn").on('click', getFacebookInfo)
+
+
+
+});
